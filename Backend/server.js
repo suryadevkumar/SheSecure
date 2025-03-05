31267bd7538ff1ffcf95a676a9e0ef32b7f0b796
Suryadev Kumar <suryadevkumar786786@gmail.com>
2025-03-05 10:45:44 +0530

    server.js modified

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
 import {Server} from 'socket.io';
+import session from 'express-session';
+import cors from 'cors';
 
-import connectDB from './db/connection.js';
+import authRoutes from './routes/User';
+import connectDB from './db/connection';
 
 const app = express();
 const server=http.createServer(app);
 const io=new Server(server);
 
 dotenv.config();
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET || "my_secret_key",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 5 * 60 * 1000 },
+    })
+);
+app.use('/api/auth',authRoutes);
 
 connectDB();
 
@@ -20,7 +38,7 @@ app.get('/', (req, res) => {
         margin-top: 30px;">Server is running on PORT 5000</p>`);
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`Backend is running on http://localhost:${port}`);
