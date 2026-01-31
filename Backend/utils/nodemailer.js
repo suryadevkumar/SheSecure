40766aba65ff1be722952e3b91b7962a2c702be6
Suryadev Kumar <suryadevkumar786786@gmail.com>
2026-01-31 08:07:33 +0530

    email send code modified

--- Backend/utils/nodemailer.js
+++ Backend/utils/nodemailer.js
@@ -1,10 +1,49 @@
+// import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
+
+// const mailSender = async (email, subject, body) => {
+//     const mailOptions = {
+//         from: "SheSecure <noreply@shesecure.com>",
+//         to: email,
+//         subject: `${subject}`,
+//         html: `${body}`
+//     };
+
+//     return new Promise((resolve, reject) => {
+//         transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//                 console.error('Error while sending email:', error);
+//                 return reject(error);
+//             } else {
+//                 console.log('Email sent:', info.response);
+//                 return resolve(info);
+//             }
+//         });
+//     });
+// };
+
+// export default mailSender;
+
+
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
